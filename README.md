cd21b03537858450220f8dd9fc97fecdc4cbdbbd
Spencer Williams <spencer@aninternetpresence.net>
2020-01-19 13:55:52 -0600

    Add buttons and more lights
      * standbyLight indicates that the system is ready
      * Press the startButton to arm the system. The armingLight will
        pulse until the armedLight is on. Then the system will detect
        motion.
      * Upon detecting motion, a video is recorded for ten seconds. The
        recordingLight will blink
      * Press the stopButton to go back to the intialized (standby)
        state. If a video is queued to be uploaded, it will not be.
        Currently uploading videos will continue to upload.
      * Hold down the stopButton for five seconds to turn off the
        program.

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ This script is based on the [Parent Detector](https://www.raspberrypi.org/learni
 
 Make sure the camera is connected and the [picamera](https://picamera.readthedocs.io/en/release-1.12/) module is enabled. [See this guide](https://www.raspberrypi.org/learning/getting-started-with-picamera/worksheet/) for getting set up.
 The motion detector should be connected on GPIO 4 (for Raspyberry Pi 3).
+
+## Run as a script in a virtual environment
+
+    python -m venv venv
+
+Create a virtual environment for this project in a folder called `venv`. Use that to install all the dependencies and run the script
--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 #! venv/bin/python
 
-from gpiozero import MotionSensor, LED
+from gpiozero import MotionSensor, LED, PWMLED, Button
 from picamera import PiCamera
 from datetime import datetime
 from subprocess import run
 from threading import Thread
 from time import sleep
 import os
+
+camera = PiCamera()
+pir = MotionSensor(4)
+standbyLight = LED(27)
+armingLight = PWMLED(22)
+armedLight = LED(18)
+recordingLight = PWMLED(23)
+startButton = Button(24)
+stopButton = Button(25, hold_time=5)
+VIDEOS_DIR = os.path.expanduser(os.path.join('~', 'Videos'))
+os.chdir(VIDEOS_DIR)
+idCounter = 0
+exitFlag = 0
+running = 0
+initialized = 0
 
 
 class DropboxThread(Thread):
@@ -19,6 +34,9 @@ class DropboxThread(Thread):
 
     def run(self):
         print("Uploading " + self.videoFilePath)
+        if exitFlag or not running:
+            print('Not uploading file')
+            return
         try:
             run(['dropbox_uploader.sh', 'upload', self.videoFilePath, self.dropboxSavePath])
         except Exception as exp:
@@ -26,74 +44,130 @@ class DropboxThread(Thread):
         print("Uploaded " + self.videoFilePath)
 
 
-camera = PiCamera()
-pir = MotionSensor(4)
-recordLight = LED(17)
-VIDEOS_DIR = os.path.expanduser(os.path.join('~', 'Videos'))
-os.chdir(VIDEOS_DIR)
-idCounter = 0
+def init():
+    global exitFlag
+    global running
+    global initialized
 
-# Detect motion and record video while motion is detected
-while True:
-    try:
-        pir.wait_for_motion()
-        print('Motion detected')
+    if initialized:
+        print("Already initialized")
+        return
 
-        # Create year and month folder if not exists
-        current_year = datetime.now().strftime("%Y")
-        current_month = datetime.now().strftime("%m")
-        current_day = datetime.now().strftime("%d")
+    while not running:
+        initialized = 1
 
-        year_dir = os.path.join(VIDEOS_DIR, current_year)
-        if not os.path.isdir(year_dir):
-            os.mkdir(year_dir)
+        pir.when_motion = None
+        armedLight.off()
+        standbyLight.on()
 
-        month_dir = os.path.join(year_dir, current_month)
-        if not os.path.isdir(month_dir):
-            os.mkdir(month_dir)
+        print('Press the start button to arm')
 
-        save_dir = os.path.join(month_dir, current_day)
-        if not os.path.isdir(save_dir):
-            os.mkdir(save_dir)
+        while not(running or exitFlag):
+            pass
 
-        os.chdir(save_dir)
+        if exitFlag:
+            return
 
-        # Set file path for video
-        filename = datetime.now().strftime("%H.%M.%S")
-        videoFileName = os.path.join(save_dir, filename)
-        tempVideoFile = videoFileName + '.h264'
-        videoFile = videoFileName + '.mp4'
+        start()
 
-        print('Start recording video')
-        camera.start_recording(tempVideoFile)
-        recordLight.on()
-        #pir.wait_for_no_motion()
-        sleep(10)
+def motionDetected():
+    global idCounter
+    if exitFlag or not running:
+        return
 
-        # Motion no longer detected
+    print('Motion detected')
+
+    # Create year and month folder if not exists
+    current_year = datetime.now().strftime("%Y")
+    current_month = datetime.now().strftime("%m")
+    current_day = datetime.now().strftime("%d")
+
+    year_dir = os.path.join(VIDEOS_DIR, current_year)
+    if not os.path.isdir(year_dir):
+        os.mkdir(year_dir)
+
+    month_dir = os.path.join(year_dir, current_month)
+    if not os.path.isdir(month_dir):
+        os.mkdir(month_dir)
+
+    save_dir = os.path.join(month_dir, current_day)
+    if not os.path.isdir(save_dir):
+        os.mkdir(save_dir)
+
+    os.chdir(save_dir)
+
+    # Set file path for video
+    filename = datetime.now().strftime("%H.%M.%S")
+    videoFileName = os.path.join(save_dir, filename)
+    tempVideoFile = videoFileName + '.h264'
+    videoFile = videoFileName + '.mp4'
+
+    print('Start recording video')
+    recordingLight.pulse()
+    camera.start_recording(tempVideoFile)
+    # pir.wait_for_no_motion()
+    sleep(10)
+
+    # Motion no longer detected
+    camera.stop_recording()
+    recordingLight.off()
+
+    # If the video file was saved convert it to MP4
+    if os.path.isfile(tempVideoFile):
+        try:
+            run(['MP4Box', '-fps', '30', '-add', tempVideoFile, videoFile])
+            os.remove(tempVideoFile);
+        except Exception as exp:
+            print('There was a problem while converting ', videoFile, ': ', exp.args)
+
+        filename = os.path.basename(videoFile)
+
+        idCounter += 1
+
+        dropboxUploadThread = DropboxThread(idCounter, filename, videoFile,
+                                            os.path.join(current_year, current_month, current_day, filename))
+        dropboxUploadThread.start()
+
+def start():
+    global exitFlag
+    global running
+    global initialized
+
+    standbyLight.off()
+    initialized = 0
+    print("Arming...")
+    armingLight.pulse()
+    sleep(5)
+    armingLight.off()
+
+    armedLight.on()
+    print('Armed.')
+    print('Waiting for motion...')
+    pir.when_motion = motionDetected
+
+    while not exitFlag and running:
+        pass
+
+def end():
+    global exitFlag
+    print("Security program ending")
+    exitFlag = 1
+
+    if camera.recording:
         camera.stop_recording()
-        recordLight.off()
 
-        # If the video file was saved convert it to MP4
-        if os.path.isfile(tempVideoFile):
-            try:
-                run(['MP4Box', '-fps', '30', '-add', tempVideoFile, videoFile])
-                os.remove(tempVideoFile);
-            except Exception as exp:
-                print('There was a problem while converting ', videoFile, ': ', exp.args)
+    exit()
 
-            filename = os.path.basename(videoFile)
+def enableRunning():
+    global running
+    running = 1
 
-            idCounter += 1
+def disableRunning():
+    global running
+    running = 0
 
-            dropboxUploadThread = DropboxThread(idCounter, filename, videoFile,
-                                                os.path.join(current_year, current_month, current_day, filename))
-            dropboxUploadThread.start()
-    except:
-        print("Security program ending")
+startButton.when_pressed = enableRunning;
+stopButton.when_pressed = disableRunning
+stopButton.when_held = end
 
-        recordLight.off()
-
-        if camera.recording:
-            camera.stop_recording()
-        exit()
+init()
