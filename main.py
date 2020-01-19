54ccf5c2c694693e8052ec28f9a343476ef893ac
Spencer Williams <spencer@aninternetpresence.net>
2020-01-18 20:59:37 -0600

    Update for python3 and add default record time
      * Sum methods had to be replaced.
      * Record for ten seconds after motion is detected. There is too
        much of a gap between the lowest timing and the notch right
        above
      * Show a light when the system is recording video

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
-from gpiozero import MotionSensor
+#! venv/bin/python
+
+from gpiozero import MotionSensor, LED
 from picamera import PiCamera
 from datetime import datetime
-from subprocess import call
-from subprocess import check_call
+from subprocess import run
 from threading import Thread
+from time import sleep
 import os
 
-class dropboxThread (Thread):
+
+class DropboxThread(Thread):
     def __init__(self, threadID, name, videoFilePath, dropboxSavePath):
         Thread.__init__(self)
         self.threadID = threadID
         self.name = name
         self.videoFilePath = videoFilePath
         self.dropboxSavePath = dropboxSavePath
+
     def run(self):
-        print "Uploading " + self.videoFilePath
+        print("Uploading " + self.videoFilePath)
         try:
-        	check_call(['dropbox_uploader.sh','upload', self.videoFilePath, self.dropboxSavePath])    
+            run(['dropbox_uploader.sh', 'upload', self.videoFilePath, self.dropboxSavePath])
         except Exception as exp:
-                print('There was a problem uploading to Dropbox: ', self.videoFilePath, ': ', exp.args)
-        print "Uploaded " + self.videoFilePath
-	
+            print('There was a problem uploading to Dropbox: ', self.videoFilePath, ': ', exp.args)
+        print("Uploaded " + self.videoFilePath)
+
+
 camera = PiCamera()
 pir = MotionSensor(4)
+recordLight = LED(17)
 VIDEOS_DIR = os.path.expanduser(os.path.join('~', 'Videos'))
 os.chdir(VIDEOS_DIR)
 idCounter = 0
@@ -31,7 +37,7 @@ idCounter = 0
 while True:
     try:
         pir.wait_for_motion()
-        # Motion detected
+        print('Motion detected')
 
         # Create year and month folder if not exists
         current_year = datetime.now().strftime("%Y")
@@ -42,44 +48,52 @@ while True:
         if not os.path.isdir(year_dir):
             os.mkdir(year_dir)
 
-        month_dir = os.path.join(year_dir, current_month)    
+        month_dir = os.path.join(year_dir, current_month)
         if not os.path.isdir(month_dir):
             os.mkdir(month_dir)
-            
-        save_dir = os.path.join(month_dir, current_day)    
+
+        save_dir = os.path.join(month_dir, current_day)
         if not os.path.isdir(save_dir):
             os.mkdir(save_dir)
-            
+
         os.chdir(save_dir)
 
-		# Set file path for video
+        # Set file path for video
         filename = datetime.now().strftime("%H.%M.%S")
         videoFileName = os.path.join(save_dir, filename)
         tempVideoFile = videoFileName + '.h264'
         videoFile = videoFileName + '.mp4'
-        
-        # Start recording video
-        camera.start_recording(tempVideoFile) 
-        pir.wait_for_no_motion()
+
+        print('Start recording video')
+        camera.start_recording(tempVideoFile)
+        recordLight.on()
+        #pir.wait_for_no_motion()
+        sleep(10)
 
         # Motion no longer detected
         camera.stop_recording()
-        
+        recordLight.off()
+
         # If the video file was saved convert it to MP4
         if os.path.isfile(tempVideoFile):
             try:
-              check_call(['MP4Box','-fps', '30', '-add', tempVideoFile, videoFile])
-              os.remove(tempVideoFile);
-            except(Exception, Argument):
-                print('There was a problem while converting ', videoFile, ': ', Argument)
-                
+                run(['MP4Box', '-fps', '30', '-add', tempVideoFile, videoFile])
+                os.remove(tempVideoFile);
+            except Exception as exp:
+                print('There was a problem while converting ', videoFile, ': ', exp.args)
+
             filename = os.path.basename(videoFile)
-            
+
             idCounter += 1
-            
-            dropboxUploadThread = dropboxThread(idCounter, filename, videoFile, os.path.join(current_year, current_month, current_day, filename))
+
+            dropboxUploadThread = DropboxThread(idCounter, filename, videoFile,
+                                                os.path.join(current_year, current_month, current_day, filename))
             dropboxUploadThread.start()
     except:
         print("Security program ending")
+
+        recordLight.off()
+
         if camera.recording:
-          camera.stop_recording()
+            camera.stop_recording()
+        exit()
